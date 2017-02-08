a06e399e3d6050fc0b284e2ffaf82eed3c04f485
JALops <jalaflin@gmail.com>
2017-02-07 19:40:09 -0800

    added print date

--- src/edu/pcc/cis/hello_world/HelloWorld.java
+++ src/edu/pcc/cis/hello_world/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.hello_world;
 
+import java.util.Date;
+
 /**
  * @Author: Joshua Laflin
  * @Version: 2/7/17
@@ -8,6 +10,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 
     }
